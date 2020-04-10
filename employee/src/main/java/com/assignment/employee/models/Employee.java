666d60338a9f489504f4a4df9aa817258ede180f
dassusobhan <63341093+dassusobhan@users.noreply.github.com>
2020-04-10 23:12:03 +0530

    Update Employee.java
    Changes made

--- employee/src/main/java/com/assignment/employee/models/Employee.java
+++ employee/src/main/java/com/assignment/employee/models/Employee.java
@@ -13,21 +13,21 @@ public class Employee implements Serializable {
 	 */
 	private static final long serialVersionUID = -7337786246939689218L;
 
-	@NotNull(message = "Employee No should not be null") 
+	@NotNull(message = "Employee No should not be null")
 	private Integer emp_no;
 
-	@NotNull(message = "Employee No should not be null") 
-	@Past(message = "Birth Date should be of past date") 
+	@NotNull(message = "Employee No should not be null")
+	@Past(message = "Birth Date should be of past date")
 	private Date birth_date;
 
 	@NotNull(message = "FirstName should not be null")
 	private String first_name;
-	
+
 	@NotNull(message = "LastName should not be null")
 	private String last_name;
 
-	/* private String gender; ENUM ('M','F'); */
-	@NotNull(message = "Employee Hire date should not be null") 
+	private String gender; /* ENUM ('M','F'); */
+	@NotNull(message = "Employee Hire date should not be null")
 	private Date hire_date;
 
 	public Employee() {
@@ -35,14 +35,14 @@ public class Employee implements Serializable {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Employee(Integer emp_no, Date birth_date, String first_name, String last_name,
+	public Employee(Integer emp_no, Date birth_date, String first_name, String last_name, String gender,
 			Date hire_date) {
 		super();
 		this.emp_no = emp_no;
 		this.birth_date = birth_date;
 		this.first_name = first_name;
 		this.last_name = last_name;
-		/* this.gender = gender; */
+		this.gender = gender;
 		this.hire_date = hire_date;
 	}
 
@@ -78,11 +78,14 @@ public class Employee implements Serializable {
 		this.last_name = last_name;
 	}
 
-	/*
-	 * public String getGender() { return gender; }
-	 * 
-	 * public void setGender(String gender) { this.gender = gender; }
-	 */
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
 	public Date getHire_date() {
 		return hire_date;
 	}
